a1a66c35db2cb8d743b825f9d39cae0580b5d458
Juanpiq <juanavarroquintero@gmail.com>
2025-03-07 00:54:35 -0500

    Search funcionando

--- src/app/TodoSearch.js
+++ src/app/TodoSearch.js
@@ -1,10 +1,20 @@
 "use client";
-function TodoSearch(){
+import React from "react";
+function TodoSearch({
+  searchValue,
+  setSearchValue
+}){
+  
     return(
         <input className="border border-2 border-black w-64 mx-auto 
                           focus:border-blue-500 focus:ring-2 focus:ring-blue-300 focus:outline-none" 
                placeholder="Cortar cebolla"
-               onChange={(event) => console.log(event.target.value)}
+               value = {searchValue}
+               onChange={
+                (event) => {
+                  setSearchValue(event.target.value)
+                }
+               }
         />
     );
   }
--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,54 @@
+"use client"
 import Image from "next/image";
 import { TodoCounter } from "./TodoCounter";
 import { TodoList } from "./TodoList";
 import { TodoItem } from "./TodoItem";
 import { TodoSearch } from "./TodoSearch";
 import TodoFloatingButton from "./TodoFloatingButton"
+import React from "react";
+
+const defaultTodos = [
+  {text: 'Cortar cebolla', completed: true},
+  {text: 'Tomar el Curso de Intro a React.js', completed: true},
+  {text: 'El ayuwoki', completed: false},
+  {text: 'Llorar con la llorona', completed: false}
+]
 
 export default function Home() {
+  const[todos, setTodos] = React.useState(defaultTodos); //estado para los elementos de la lista
+  const [searchValue, setSearchValue] = React.useState(''); //estado para el search (input)
+
+  const completedTodos = todos.filter(todo => todo.completed).length; //devuelve el tamaño del array de los elementos que cumplan con la condicion, con tal de que tenga un objeto este devuelve true
+  const totalTodos = todos.length;
+
+  const searchedTodos = todos.filter(
+    (todo) =>{
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todo.text.includes(searchText);
+    } )
+
+  console.log(searchValue);
   return (
     <>
     <div className="flex justify-center flex flex-col m-5 gap-10">
-      <TodoCounter total={18} completed={5}/>
+      
 
-      <TodoSearch/>
+      <TodoCounter total={totalTodos} completed={completedTodos}/>
+
+      <TodoSearch
+        searchValue = {searchValue}
+        setSearchValue = {setSearchValue}
+      />
 
       <TodoList>
-        <TodoItem text={"Cortar cebollas"}/>
-        <TodoItem text={"Hola Mundo"}/>
-        <TodoItem text={"Maracuyá"}/>
+        {searchedTodos.map(todo =>(
+          <TodoItem
+            key = {todo.text}
+            text = {todo.text}
+            completed = {todo.completed}
+          />
+        ))}
       </TodoList>
 
       <TodoFloatingButton/>
